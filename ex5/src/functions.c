21310da28becd88b0d52d3d509dbc5235abf46a3
Jose Alejandro <jacorderopr@gmail.com>
2024-09-26 22:35:32 -0400

    la semilla loko GUAOOO

--- ex5/src/functions.c
+++ ex5/src/functions.c
@@ -1,6 +1,5 @@
 #include "functions.h"
 
-
 BallPosition ball_pos_array[10];
 int ball_pos_array_index = 0;
 
@@ -9,85 +8,91 @@ int dx, dy;
 int row, col;
 int break_loop = 0;
 
+void *UpdateAllBallPos(void *)
+{
+  dx = dy = 1; // tal vez hay que hacer uno para cada bola
+  while (!break_loop)
+  {
 
+    clear();
 
-void* UpdateAllBallPos(void*){
-    dx = dy = 1; // tal vez hay que hacer uno para cada bola
-while(!break_loop){
+    mvprintw(0, 0, "%d", counter_val); // displays the counter, DOES NOT update it!
+    getmaxyx(stdscr, row, col);        /* Obtiene el numbero de filas y columnas */
 
-  clear();
-  
-  mvprintw(0, 0, "%d", counter_val); // displays the counter, DOES NOT update it!
-  for(int i = 0; i < ball_pos_array_index && ball_pos_array != NULL; i++){
-    getmaxyx(stdscr, row, col); /* Obtiene el numbero de filas y columnas */
-    UpdateBallPos(i);
-  }
-  refresh();
+    for (int i = 0; i < ball_pos_array_index; i++)
+    {
+      // getmaxyx(stdscr, row, col); /* Obtiene el numbero de filas y columnas */
+      UpdateBallPos(i);
+    }
+    refresh();
     usleep(100000); /* Duerme por 100ms */
-}
-
-}
-
-void UpdateBallPos(int ball_pos_index){
-
-     mvprintw(ball_pos_array[ball_pos_index].y, ball_pos_array[ball_pos_index].x, "o");
-   
-    ball_pos_array[ball_pos_index].x += dx;
-    ball_pos_array[ball_pos_index].y += dy;
-    if (ball_pos_array[ball_pos_index].x >= col - 1) {
-      dx = -1;
-    }
-    if (ball_pos_array[ball_pos_index].y >= row - 1) {
-      dy = -1;
-    }
-    if (ball_pos_array[ball_pos_index].x <= 1) {
-      dx = 1;
-    }
-    if (ball_pos_array[ball_pos_index].y <= 1) {
-      dy = 1;
-    }
-
-}
-
-
-void* UpdateCounter(void*){
-
-  while (!break_loop) {
-    
-   counter_val++;
-    usleep(1000000); /* Duerme por 1sg*/
-
   }
-
 }
 
-void* GetCommands(void*){
-    int key_stroke;
+void UpdateBallPos(int ball_pos_index)
+{
 
+  mvprintw(ball_pos_array[ball_pos_index].y, ball_pos_array[ball_pos_index].x, "o");
 
-  while (!break_loop) {
+  ball_pos_array[ball_pos_index].x += dx;
+  ball_pos_array[ball_pos_index].y += dy;
+  if (ball_pos_array[ball_pos_index].x >= col - 1)
+  {
+    dx = -1;
+  }
+  if (ball_pos_array[ball_pos_index].y >= row - 1)
+  {
+    dy = -1;
+  }
+  if (ball_pos_array[ball_pos_index].x <= 1)
+  {
+    dx = 1;
+  }
+  if (ball_pos_array[ball_pos_index].y <= 1)
+  {
+    dy = 1;
+  }
+}
+
+void *UpdateCounter(void *)
+{
+
+  while (!break_loop)
+  {
+
+    counter_val++;
+    usleep(1000000); /* Duerme por 1sg*/
+  }
+}
+
+void *GetCommands(void *)
+{
+  int key_stroke;
+
+  while (!break_loop)
+  {
     key_stroke = getch();
     refresh();
-    switch (key_stroke) {
-      case KEY_LEFT:
-        /* code */
-        break;
-      case 'f':
-        // add a new ball
-        // if(ball_pos_array_index < 2){
+    switch (key_stroke)
+    {
+    case KEY_LEFT:
+      /* code */
+      break;
+    case 'f':
+      // add a new ball
+      if (ball_pos_array_index < 10)
+      {
 
-        //   ball_pos_array_index++;
-        //   ball_pos_array[ball_pos_array_index].x = 15;
-        //   ball_pos_array[ball_pos_array_index].y = 35;
-        // }
+        ball_pos_array[ball_pos_array_index].x = (rand() % 20) + 1;
+        ball_pos_array[ball_pos_array_index].y = (rand() % 20) + 1;
+        ball_pos_array_index++;
+      }
 
-        break;
-      case ENTER_NCURSES:
-        // case KEY_ENTER:
-        break_loop = 1;
-        break;
+      break;
+    case ENTER_NCURSES:
+      // case KEY_ENTER:
+      break_loop = 1;
+      break;
     }
   }
-
-
 }
--- ex5/src/main.c
+++ ex5/src/main.c
@@ -1,6 +1,7 @@
 #include "functions.h"
 
-int main() {
+int main()
+{
   pthread_t id1, id2, id3;
 
   initscr();
@@ -9,35 +10,30 @@ int main() {
 
   curs_set(0);
 
-srand((unsigned)time(NULL));
+  srand((unsigned)time(NULL));
 
-// siempre esta spawning la bolita en el mismo lugar no importa las coordenadas iniciales.
-for(int i = 0; i < 5; i++){
+  // siempre esta spawning la bolita en el mismo lugar no importa las coordenadas iniciales.
+  for (int i = 0; i < 5; i++)
+  {
 
-  ball_pos_array[ball_pos_array_index].x = (rand() % 20) + 1; // 1-20
-  ball_pos_array[ball_pos_array_index].y = (rand() % 10) + 1; // 1-10
+    ball_pos_array[ball_pos_array_index].x = (rand() % 20) + 1; // 1-20
+    ball_pos_array[ball_pos_array_index].y = (rand() % 10) + 1; // 1-10
 
-  printf("ball index: %d x: %d, y: %d\n", i, ball_pos_array[ball_pos_array_index].x, ball_pos_array[ball_pos_array_index].y);
+    printf("ball index: %d x: %d, y: %d\n", i, ball_pos_array[ball_pos_array_index].x, ball_pos_array[ball_pos_array_index].y);
 
-  ball_pos_array_index++;
+    ball_pos_array_index++;
+  }
 
-}
+  pthread_create(&id3, NULL, GetCommands, NULL);
+  pthread_create(&id1, NULL, UpdateAllBallPos, NULL);
+  pthread_create(&id2, NULL, UpdateCounter, NULL);
 
-
-pthread_create(&id3, NULL, GetCommands, NULL);
-pthread_create(&id1, NULL, UpdateAllBallPos, NULL);
-pthread_create(&id2, NULL, UpdateCounter, NULL);
-
-
-pthread_join(id3, NULL);
-pthread_join(id1, NULL);
-pthread_join(id2, NULL);
-
-
+  pthread_join(id3, NULL);
+  pthread_join(id1, NULL);
+  pthread_join(id2, NULL);
 
   getch();
   endwin();
 
   return 0;
 }
-
